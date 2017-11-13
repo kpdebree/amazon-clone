b3a5f8f3f787b90c2ef2bd2c491bdf3ab9b316d2
Kevin de Bree <kpdebree@gmail.com>
2017-11-12 23:52:43 -0800

    Customer Interface Complete

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -53,23 +53,44 @@ function displayInventory() {
 					message: "Which item would you like to purchase?"
 				},
 				{
-					name: "purchase",
+					name: "quantity",
 					type: "input",
 					message: "How many units would you like to purchase?"
 				}
 			]).then(function(answer) {
+				var quantity = answer.quantity;
 				var chosenItem;
 				for (var i = 0; i < results.length; i++) {
 					if (results[i].product_name === answer.choice) {
 						chosenItem = results[i]
-						console.log(chosenItem);
 					}
 				}
-			})
 
-
-		connection.end();
-	});
+					if (quantity <= chosenItem.stock_quantity) {
+						var newQuantity = chosenItem.stock_quantity - quantity;
+						connection.query(
+							"UPDATE products SET ? WHERE ?",
+							[
+								{
+									stock_quantity: newQuantity
+								},
+								{
+									item_id: chosenItem.item_id
+								}	 
+							],
+							function(error) {
+								if (error) throw err;
+								console.log("Purchase Order Registered");
+								start_menu();
+							}
+						);
+					} 
+					else {
+						console.log("We do not have enough in stock");
+						start_menu();
+					}
+				})
+			});
 }
 
 function quitApplication() {
